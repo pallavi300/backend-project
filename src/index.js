5bae0013f74d11b2130f92abf64b637cc0a39826
pallavi300 <pallavi81@ievitableinfotech.com>
2024-07-03 11:20:46 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config({
 
 connectDB()
 .then(()  =>{
-    app.listen(process.env.PORT || 8000, () =>{
+    app.listen(process.env.PORT || 3000, () =>{
         console.log(`Server is running at port : ${process.env.PORT}`);
         app.on("error", (error)=>{
             console.log("ERRR: ", error);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 // for promisses
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+   return  (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next))
             .catch((err) => next(err))
     }
@@ -19,7 +19,7 @@ export { asyncHandler }
 
 //for trycatch
 
-// const asyncHandler = (fn) => async (req, res, next,) => {
+// const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
 //         await fn(req, res, next)
 //     } catch (error) {
